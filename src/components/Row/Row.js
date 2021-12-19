5bfc171d9a5c2c422fe88034f3e74440c8fb81bf
Murat Can ŞAHİN <muratcansahin10@gmail.com>
2021-12-19 21:49:06 +0300

    create rows

---
+++ src/components/Row/Row.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./Row.css";
+const baseUrl = "https://image.tmdb.org/t/p/original";
+
+const Row = ({ title, fetchUrl, isLargeRow }) => {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"} `}
+            src={`${baseUrl}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Row;
